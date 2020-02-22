cdc6bc34006a9a15c118e949de3f9b2291789166
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-02-22 17:40:04 +0900

    Chapter 01

---
+++ SelfStudyJava/chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,19 @@
+package sec03.exam01; 
+/* Hello.java는 sec03.exam01 패키지에서 생성되었기 때문에
+ * 이와 같은 package 선언이 반드시 있어야 합니다. */ 
+
+/*
+ 작성자 : 박서윤
+ 작성일 : 2020.02.22
+*/
+
+public class Hello // class 선언부. (class 이름은 소스파일명과 동일해야 함.)
+{ // class block
+
+	// program entry point
+	public static void main(String[] args) // method 선언부
+	{ // method block
+		System.out.println("Hello, Java");
+	}
+
+}
---
+++ SelfStudyJava/chap01/src/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,17 @@
+package sec03.exam02;
+/*
+ * Author : Seoyoon Park
+ * Date   : 2020.02.22
+*/
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		int x;
+		x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
