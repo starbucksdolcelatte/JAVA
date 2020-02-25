2c91b0ecd7142510c80703cfa81048a3c2695991
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-02-25 22:42:16 +0900

    논리 연산

---
+++ SelfStudyJava/chap03/src/sec02/exam03/DenyLogicOperatorExample.java
@@ -0,0 +1,19 @@
+package sec02.exam03;
+/*
+ * Author : StarbucksDolceLatte
+ * Date   : 2020.02.25
+*/
+
+public class DenyLogicOperatorExample {
+	public static void main(String[] args)
+	{ 
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+}
