c12d34259fd37f716d20e73daa71df5eb2bda8c0
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-02-25 22:40:47 +0900

    숫자 연산

---
+++ SelfStudyJava/chap03/src/sec02/exam04/ArithmeticOperatorExample.java
@@ -0,0 +1,27 @@
+package sec02.exam04;
+/*
+ * Author : StarbucksDolceLatte
+ * Date   : 2020.02.25
+*/
+
+public class ArithmeticOperatorExample {
+	public static void main(String[] args)
+	{ 
+		int v1 = 5;
+		int v2 = 2;
+		
+		int result1 = v1 + v2;
+		System.out.println("result=" + result1);
+		int result2 = v1 - v2;
+		System.out.println("result=" + result2);
+		int result3 = v1 * v2;
+		System.out.println("result=" + result3);
+		int result4 = v1 / v2;
+		System.out.println("result=" + result4);
+		int result5 = v1 % v2;
+		System.out.println("result=" + result5);
+		double result6 = (double)v1 / v2;
+		System.out.println("result=" + result6);
+		
+	}
+}
