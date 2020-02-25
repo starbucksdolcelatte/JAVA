a83dafff4b2d83362d4cb0a2c6a03606f44680de
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-02-25 21:20:35 +0900

    대입 연산자

---
+++ SelfStudyJava/chap03/src/sec02/exam10/AssignmentOperatorExample.java
@@ -0,0 +1,26 @@
+package sec02.exam10;
+/*
+ * Author : StarbucksDolceLatte
+ * Date   : 2020.02.25
+*/
+
+public class AssignmentOperatorExample {
+	public static void main(String[] args)
+	{ 
+		int result = 0;
+		result += 10;
+		System.out.println("result = " + result);
+		
+		result -= 5;
+		System.out.println("result = " + result);
+		
+		result *= 3;
+		System.out.println("result = " + result);
+		
+		result /= 5;
+		System.out.println("result = " + result);
+		
+		result %= 3;
+		System.out.println("result = " + result);
+	}
+}
