9422927480599f3441f4881e63325ff864bbdd2c
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-02-29 21:28:25 +0900

    Car class

---
+++ SelfStudyJava/chap06/src/sec03/exam01/Car.java
@@ -0,0 +1,16 @@
+package sec03.exam01;
+/*
+작성자 : StarbucksDolceLatte
+작성일 : 2020.02.29
+*/
+public class Car {
+	//필드
+	String color;
+	int cc;
+	
+	//생성자
+	public Car(String car_color, int car_cc){
+		this.color = car_color;
+		this.cc = car_cc;
+	}
+}
---
+++ SelfStudyJava/chap06/src/sec03/exam01/CarExample.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+/*
+작성자 : StarbucksDolceLatte
+작성일 : 2020.02.29
+*/
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("black", 3000);
+		System.out.println("myCar.color= " + myCar.color);
+		System.out.println("myCar.cc = " + myCar.cc);
+	}
+
+}
