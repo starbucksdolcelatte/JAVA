44d5e41787e9d26312adbd9680aef64d057a927b
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-02-29 20:10:28 +0900

    array.length

---
+++ SelfStudyJava/chap05/src/sec02/exam04/ArrayLengthExample.java
@@ -0,0 +1,22 @@
+package sec02.exam04;
+/*
+ * Author : StarbucksDolceLatte
+ * Date   : 2020.02.29
+*/
+
+public class ArrayLengthExample {
+
+	public static void main(String[] args) {
+		// length 필드는 읽기 전용이라서 값을 바꿀 수 없다.
+		int[] scores = {83, 90, 87};
+		
+		int sum = 0;
+		for (int i=0; i<scores.length; i++)
+			sum += scores[i];
+		System.out.println("총합 : " + sum);
+		
+		double avg = (double) sum / scores.length;
+		System.out.println("평균 : " + avg);
+	}
+
+}
