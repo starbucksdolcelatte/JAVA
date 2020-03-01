c330fbd61cd9d00f8a82be798c1fed8055eff0bb
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-03-01 20:24:03 +0900

    method

---
+++ SelfStudyJava/chap06/src/sec05/exam01/Car.java
@@ -0,0 +1,29 @@
+package sec05.exam01;
+/*
+작성자 : StarbucksDolceLatte
+작성일 : 2020.02.29
+*/
+public class Car {
+
+	//필드
+	String model;
+	int speed;
+	
+	//생성자
+	Car(String model){
+		this.model = model;
+	}
+	
+	//메소드
+	void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	void run() {
+		for(int i=0; i<=50; i+=10) {
+			this.setSpeed(i);
+			System.out.println(this.model + "가 달립니다. (시속 "+this.speed + "km/h)");
+		}
+	}
+
+}
---
+++ SelfStudyJava/chap06/src/sec05/exam01/CarExample.java
@@ -0,0 +1,15 @@
+package sec05.exam01;
+/*
+작성자 : StarbucksDolceLatte
+작성일 : 2020.02.29
+*/
+public class CarExample {
+
+	public static void main(String[] args) {
+		
+		Car myCar = new Car("포르쉐");
+		
+		myCar.run();
+	}
+
+}
